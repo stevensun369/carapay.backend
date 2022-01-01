43bdace5333d58ce79f6cd5ef4964bd1c50fa97c
Steven <stevensuncatalin369@gmail.com>
2022-01-01 12:58:40 +0200

    got around to deploying

--- db/db.go
+++ db/db.go
@@ -6,15 +6,13 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-
-	"backend/env"
 )
 
 var Client *mongo.Client
 
 func InitDatabase() {
   var err error
-  Client, err = mongo.Connect(context.Background(), options.Client().ApplyURI(env.MongoURI))
+  Client, err = mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb+srv://stevensun:stevensun@dev.zn9a9.mongodb.net/carapay?retryWrites=true&w=majority"))
 
   if err != nil {
     log.Fatal(err)
